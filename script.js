a789af0c5d2b1a67cb2e4b0de370c15d3d5111e1
whats4breakfast <65098577+whats4breakfast@users.noreply.github.com>
2020-05-10 15:59:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 function myFunction() {
     document.getElementById("a").innerHTML = "New text, yay!";
 }
+
+function imageCache(base, firstNum, lastNum) {
+    this.cache = [];
+    var img;
+    for (var i = firstNum; i <= lastnum; i++) {
+        img = new Image();
+        img.src = base + i + ".jpg";
+        this.cache.push(img);
+    }
+}
+
+imageCache.prototype.nextImage(id) {
+    var element = document.getElementById(id);
+    var targetSrc = element.src;
+    var cache = this.cache;
+    for (var i = 0; i < cache.length; i++) {
+        if (cache[i].src) === targetSrc) {
+            i++;
+            if (i >= cache.length) {
+                i = 0;
+            }
+            element.src = cache[i].src;
+            return;
+        }
+    }
+}
