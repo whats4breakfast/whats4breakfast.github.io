1d673719b88334f5efda1f87dd3b5b8cfd265d3b
Nick <nicholasereyes@gmail.com>
2020-05-12 21:44:59 -0700

    d3 test

--- tut.js
+++ tut.js
@@ -1,15 +1,50 @@
-d3.select("#list").selectAll("li")
-    .data([10, 20, 30, 25, 15])
-    .text(function(d) 
-     { return "This is pre-existing element and the value is " + d; })
-    .enter()
-    .append("li")
-    .text(function(d) 
-     { return "This is dynamically created element and the value is " + d; });
+var svg = d3.select("svg"),
+width = svg.attr("width"),
+height = svg.attr("height"),
+radius = Math.min(width, height) / 2;
 
-    function remove() {
-      d3.selectAll("li")
-      .data([10, 20, 30, 15])
-      .exit()
-      .remove()
-    }
+var g = svg.append("g")
+.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+var color = d3.scaleOrdinal([
+	'gray', 'green', 'brown', 'orange', 'yellow', 'red', 'purple'
+	]);
+
+var pie = d3.pie().value(function(d) { 
+	return d.percent; 
+});
+
+var path = d3.arc()
+.outerRadius(radius - 10).innerRadius(0);
+
+var label = d3.arc()
+.outerRadius(radius).innerRadius(radius - 80);
+
+d3.csv("populations.csv", function(error, data) {
+	if (error) {
+		throw error;
+	}
+	
+	var arc = g.selectAll(".arc")
+	.data(pie(data))
+	.enter()
+	.append("g")
+	.attr("class", "arc");
+	
+	arc.append("path")
+	.attr("d", path)
+	.attr("fill", function(d) { return color(d.data.states); });
+	
+	console.log(arc)
+	
+	arc.append("text").attr("transform", function(d) { 
+		return "translate(" + label.centroid(d) + ")"; 
+	})
+	
+	.text(function(d) { return d.data.states; });
+});
+
+svg.append("g")
+.attr("transform", "translate(" + (width / 2 - 120) + "," + 20 + ")")
+.append("text").text("Top population states in india")
+.attr("class", "title")
