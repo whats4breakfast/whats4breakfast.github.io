5d75d6f142f438699827b7a1f9f96b7570971415
Nick <nicholasereyes@gmail.com>
2020-07-22 23:04:36 -0700

    PULL FROM RAW DATA

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,24 +1,11 @@
-d3.csv('../data/types.csv').then(makeChart);
+d3.csv("../data/pkmn.csv").then(typeAvgStats);
 
 // bar chart
 
-function makeChart(types){
-	var typeLabels = types.map(function(d) {return d.type});
-	var countData = types.map(function(d) {return d.count});
-	var options = {
-		maintainAspectRatio:false,
-		responsive: true,
-		legend: {
-			display:false
-		},
-		scales: {
-			xAxes: [{
-				gridLines: {
-					display:false
-				}
-			}]
-		}
-	}
+/*function typeCount(pkmn){
+	
+
+
 	var typeChart = new Chart('chart', {
 		type: 'bar',
 		data: {
@@ -29,120 +16,97 @@ function makeChart(types){
 				backgroundColor: '#b3ffff'
 			}]
 		},
-		options: options
-	});
-}
-
-// gen radar chart
-/*
-var baseStatsChart = new Chart('shart', {
-	type: 'radar',
-	data: {
-		labels: ['HP', 'Attack', 'Defense', 'Sp. Attack','Sp. Defense','Speed'],
-		datasets: [{
-			data: [63.3, 73.5, 69.4, 67.4, 66.5, 69.0]
-		}]
-	},
-	options: {
-		maintainAspectRatio:false,
-		responsive: true,
-		elements: {
-			line: {
-				backgroundColor: 'rgba(147, 18, 175, 0.2)',
-				borderColor: 'rgba(147, 18, 175, 1)',
-			}
-		},
-		legend: {
-			display:false
-		},
-		scale: {
-			angleLines: {
-				display: true
+		options = {
+			maintainAspectRatio:false,
+			responsive: true,
+			legend: {
+				display:false
 			},
-			ticks: {
-				suggestedMin: 0,
-				suggestedMax: 90
-			}
-		}
-	}
-});
-
-function generateData(x) {
-	if(x == 1) {
-		return [63.3, 73.5, 69.4, 67.4, 66.5, 69.0];
-	} else if (x == 2) {
-		return [71, 70, 71.8, 65.4, 73.4, 61.6];
-	} else if (x == 3) {
-		return [65.7, 77.2, 72.6, 71.6, 69.4, 63.6];
-	} else if (x == 4) {
-		return [73.1, 82.7, 76.2, 74.6, 74.5, 70.1];
-	} else if (x == 5) {
-		return [70.3, 80.6, 72.0, 70.0, 67.7, 66.5];
-	} else if (x == 6) {
-		return [71.1, 76.5, 73.6, 76.6, 73.2, 66.7];
-	} else {
-		return [70.6, 84.6, 78, 75.3, 75.7, 64];
-	}
-}
-
-document.getElementById('gen').addEventListener('click',
-	function() {
-		baseStatsChart.data.datasets.forEach(function(dataset) {
-			dataset.data = generateData(gen.value);
-		});
-		baseStatsChart.update();
-	})*/
-
-	d3.csv("../data/stat_dist_types.csv").then(makeTSChart);
-
-	function makeTSChart(type_stats) {
-
-		function genData(x) {
-			dt = new Array(6);
-
-			for (i = 0; i < dt.length; i++) {
-				dt[i] = type_stats[i][x];
-			}
-
-			return dt;
-		}
-
-		var typeStatsChart = new Chart('phart', {
-			type: 'radar',
-			data: {
-				labels: ['HP', 'Attack', 'Defense', 'Sp. Attack','Sp. Defense','Speed'],
-				datasets: [{
-					data: genData('bug')
+			scales: {
+				xAxes: [{
+					gridLines: {
+						display:false
+					}
 				}]
+			}
+		}
+	});
+}*/
+
+function typeAvgStats(pkmn) {
+	var types = new Array(pkmn.map(function (d) {return d.type1}), pkmn.map(function (d) {return d.type2}));
+
+	function getAllIndexes(arr, val) {
+		var indexes = [], i = -1;
+		while ((i = arr.indexOf(val, i+1)) != -1){
+			indexes.push(i);
+		}
+		return indexes;
+	}
+
+	function getAvgStats(my_type) {
+
+		var idx = getAllIndexes(types["0"], my_type).concat(getAllIndexes(types["1"], my_type));
+		var [a,b,c,d,f,g] = [0,0,0,0,0,0];
+
+		hp = pkmn.map(function (d) {return d.hp});
+		atk = pkmn.map(function (d) {return d.attack});
+		def = pkmn.map(function (d) {return d.defense});
+		spa = pkmn.map(function (d) {return d.sp_attack});
+		spd = pkmn.map(function (d) {return d.sp_defense});
+		spe = pkmn.map(function (d) {return d.speed});
+
+		idx.forEach(e => {
+			a += Number(hp[e]);
+			b += Number(atk[e]);
+			c += Number(def[e]);
+			d += Number(spa[e]);
+			f += Number(spd[e]);
+			g += Number(spe[e]);
+		});
+
+		return [a/idx.length,b/idx.length,c/idx.length,
+		d/idx.length,f/idx.length,g/idx.length];
+
+	}
+
+	var avgStatsChart = new Chart('thart', {
+		type: 'radar',
+		data: {
+			labels: ['HP', 'Attack', 'Defense', 'Sp. Attack','Sp. Defense','Speed'],
+			datasets: [{
+				data: getAvgStats('bug')
+			}]
+		},
+		options: {
+			maintainAspectRatio:false,
+			responsive: true,
+			elements: {
+				line: {
+					backgroundColor: 'rgba(147, 18, 175, 0.2)',
+					borderColor: 'rgba(147, 18, 175, 1)',
+				}
 			},
-			options: {
-				maintainAspectRatio:false,
-				responsive: true,
-				elements: {
-					line: {
-						backgroundColor: 'rgba(147, 18, 175, 0.2)',
-						borderColor: 'rgba(147, 18, 175, 1)',
-					}
+			legend: {
+				display:false
+			},
+			scale: {
+				angleLines: {
+					display: true
 				},
-				legend: {
-					display:false
-				},
-				scale: {
-					angleLines: {
-						display: true
-					},
-					ticks: {
-						suggestedMin: 0,
-						suggestedMax: 140
-					}
+				ticks: {
+					suggestedMin: 0,
+					suggestedMax: 120
 				}
 			}
-		});
-		document.getElementById('type').addEventListener('click',
-			function() {
-				typeStatsChart.data.datasets.forEach(function(dataset) {
-					dataset.data = genData(type.value);
-				});
-				typeStatsChart.update();
-			}
+		}
+	});
+
+	document.getElementById('type').addEventListener('click',
+		function() {
+			avgStatsChart.data.datasets.forEach(function(dataset) {
+				dataset.data = getAvgStats(type.value);
+			});
+			avgStatsChart.update();
+		}
 		)}
