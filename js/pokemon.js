b5f8ba351ea2aa7915b14f95e755699391327e52
Nick <nicholasereyes@gmail.com>
2020-08-12 22:42:33 -0700

    catch rate chart

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,29 +1,37 @@
+d3.csv("../data/pkmn.csv").then(typeCount);
 d3.csv("../data/pkmn.csv").then(typeAvgStats);
+d3.csv("../data/pkmn.csv").then(typeGenCapRate);
 
 // bar chart
-/*
-function typeCount(pk){
+
+function typeCount(pkmn){
 	
-	const distinct = (value, index, self) => {
-		return self.indexOf(value) === index;
+	var typ = new Array(pkmn.map(function (d) {return d.type1}), pkmn.map(function (d) {return d.type2}));
+
+	var ct = {};
+
+	for (var j = 0; j < typ.length; j++) {
+		for (var i = 0; i < typ[j].length; i++) {
+			ct[typ[j][i]] = 1 + (ct[typ[j][i]] || 0);
+		}
 	}
 
-	var types1 = pk.map(function (d) {return d.type1});
+	delete ct[''];
 
-	console.log(types1.filter(distinct));
-
+	ctSortVals = Object.keys(ct).sort(function(a,b){return ct[b]-ct[a]}).map(key => ct[key]);
+	ctSortLabels = Object.keys(ct).sort(function(a,b){return ct[b]-ct[a]});
 
 	var typeChart = new Chart('chart', {
 		type: 'bar',
 		data: {
-			labels: typeLabels,
+			labels: ctSortLabels,
 			datasets: [
 			{
-				data: countData,
+				data: ctSortVals,
 				backgroundColor: '#b3ffff'
 			}]
 		},
-		options = {
+		options: {
 			maintainAspectRatio:false,
 			responsive: true,
 			legend: {
@@ -37,8 +45,8 @@ function typeCount(pk){
 				}]
 			}
 		}
-	});
-}*/
+	}); 
+}
 
 function typeAvgStats(pkmn) {
 
@@ -122,3 +130,87 @@ function typeAvgStats(pkmn) {
 			avgStatsChart.update();
 		}
 		)}
+
+	function typeGenCapRate(pkmn) {
+
+		var cap = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]];
+		var count = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]];
+
+		pkmn.forEach(e => {
+			if(e.is_legendary==0){
+				if(e.generation==1){
+					cap[0][0] += Number(e.capture_rate);
+					count[0][0] ++;
+				} else if (e.generation==2) {
+					cap[0][1] += Number(e.capture_rate);
+					count[0][1] ++;
+				} else if (e.generation==3) {
+					cap[0][2] += Number(e.capture_rate);
+					count[0][2] ++;
+				} else if (e.generation==4) {
+					cap[0][3] += Number(e.capture_rate);
+					count[0][3] ++;
+				} else if (e.generation==5) {
+					cap[0][4] += Number(e.capture_rate);
+					count[0][4] ++;
+				} else if (e.generation==6) {
+					cap[0][5] += Number(e.capture_rate);
+					count[0][5] ++;
+				} else if(e.generation==7) {
+					cap[0][6] += Number(e.capture_rate);
+					count[0][6] ++;
+				} else;
+			} else if (e.is_legendary==1){
+				if(e.generation==1){
+					cap[1][0] += Number(e.capture_rate);
+					count[1][0] ++;
+				} else if (e.generation==2) {
+					cap[1][1] += Number(e.capture_rate);
+					count[1][1] ++;
+				} else if (e.generation==3) {
+					cap[1][2] += Number(e.capture_rate);
+					count[1][2] ++;
+				} else if (e.generation==4) {
+					cap[1][3] += Number(e.capture_rate);
+					count[1][3] ++;
+				} else if (e.generation==5) {
+					cap[1][4] += Number(e.capture_rate);
+					count[1][4] ++;
+				} else if (e.generation==6) {
+					cap[1][5] += Number(e.capture_rate);
+					count[1][5] ++;
+				} else if(e.generation==7) {
+					cap[1][6] += Number(e.capture_rate);
+					count[1][6] ++;
+				} else;
+			} else;
+		});
+
+		var capGen = [cap[0].map(function(n,i){return n/count[0][i];}),cap[1].map(function(n,i){return n/count[1][i];})]
+
+		console.log(capGen);
+
+		var avgStatsChart = new Chart('bart', {
+			type: 'line',
+			data: {
+				labels: ['Gen 1', 'Gen 2', 'Gen 3', 'Gen 4','Gen 5','Gen 6','Gen 7'],
+				datasets: [{
+					label: 'Non-Legendary',
+					borderColor: 'rgba(255, 99, 99)',
+					backgroundColor: 'rgba(0, 0, 0, 0)',
+					data: capGen[0]
+				},
+				{
+					label: 'Legendary',
+					borderColor: 'rgba(60, 179, 113)',
+					backgroundColor: 'rgba(0, 0, 0, 0)',
+					data: capGen[1]
+				}]
+			},
+			options: {
+				maintainAspectRatio:false
+				
+			}
+
+		});
+	}
